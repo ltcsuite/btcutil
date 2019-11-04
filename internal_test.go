cbb9e7f65beaa5c53e1541acae35af3119c7e2ec
iuouiyiuty <392111422@qq.com>
2019-11-04 15:16:14 +0800

    fix import

--- internal_test.go
+++ internal_test.go
@@ -12,9 +12,9 @@ interface. The functions are only exported while the tests are being run.
 package ltcutil
 
 import (
-	"github.com/Roasbeef/ltcutil/bech32"
 	"github.com/ltcsuite/ltcd/btcec"
 	"github.com/ltcsuite/ltcutil/base58"
+	"github.com/ltcsuite/ltcutil/bech32"
 	"golang.org/x/crypto/ripemd160"
 )
 
